99f496737623e9f17a39c0559ca4a7dd4cd550a4
Rimwor <rimwor@outlook.com>
2024-04-23 12:27:26 +0200

    routing files and updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,24 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, RouterModule],
   template: `
   <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.png" alt="logo" aria-hidden="true" id="logo">
-    </header>
+  <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.png" alt="logo" aria-hidden="true" id="logo">
+      </header>
+  </a>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>
-`,
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
